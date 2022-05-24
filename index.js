411b1fe184de2cef23db399fdb1a9c17cca4f66e
TrevourGuild <trevorr.guildd@gmail.com>
2022-05-24 12:18:47 -0700

    created models and auth encryption

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require("express")
 const app = express()
 const mongoose = require("mongoose")
 const dotenv = require("dotenv")
+const userRoute = require("./routes/user")
+const authRoute = require("./routes/auth")
 
 dotenv.config()
 
@@ -11,6 +13,10 @@ mongoose.connect(process.env.MONGO_URL)
     console.log(err)
 })
 
-app.listen(process.env.PORT || 5000, ()=>{
+app.use(express.json())
+app.use("/api/auth", authRoute)
+app.use("/api/users", userRoute)
+
+app.listen(process.env.PORT, ()=>{
     console.log("Backend server is running")
 })
